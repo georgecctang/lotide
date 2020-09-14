48915128af95a6fd88df7141a08ce673763ed2e9
George Tang <georgecctang@gmail.com>
2020-09-14 20:21:33 +0000

    added test cases

--- assertEqual.js
+++ assertEqual.js
@@ -3,10 +3,16 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌❌❌ Assertion failed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+// Identical strings
+assertEqual("apples", "apples");
+// Non-identical strings
+assertEqual("apples", "oranges");
+// Identical numbers
 assertEqual(1, 1);
+// Non-identical numbers
+assertEqual(1, 2);
